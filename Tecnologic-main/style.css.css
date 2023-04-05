039fbb3216a548673b5961e551e9f5283bb040a2
jocelenesilva <129219615+jocelenesilva@users.noreply.github.com>
2023-04-05 10:54:16 -0300

    Add files via upload

---
+++ Tecnologic-main/style.css.css
@@ -0,0 +1,69 @@
+#banner {
+    width:100%;
+}
+
+.principal{
+Background #cccccc;
+padding: 30px;
+}
+
+.titulo principal{
+padding-left: 20px;
+}
+
+.titulo-centralizado{
+    text-align: center;
+}
+
+p{
+ text-align: center;
+}
+
+#missao {
+font-size 20px;
+}
+
+em strong {
+color: #FF0000;
+}
+
+.itens{
+    font-style: italic;
+}
+
+conteúdos{
+    background:#FFFFFF;
+    padding: 20px;
+}
+
+ul {
+display: inline-block;
+vertical-align: top;
+width: 20px;
+margin: 15%;
+}
+
+imagconteúdo{
+    width: 50px;
+ }
+
+ color: {
+red;
+yellow;
+blue;  
+}
+
+
+  RGB:{
+    0,1,2,3,...255;
+0= ausÃªncia;
+255= mÃ¡ximo;
+}
+
+RGb: { 255,255,255};
+Rgb{0,0,255};   
+}
+
+
+
+
